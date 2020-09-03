1541d4336ebffad27367ed47a1c57634803e0ff8
porjesuraj <porjesuraj5@gmail.com>
2020-09-03 17:55:57 +0530

    assignment-2 pizza table put and delete  done

--- Assignments/assignment2/PIZZA_table.js
+++ Assignments/assignment2/PIZZA_table.js
@@ -92,6 +92,84 @@ app.post('/pizza',(request,response) =>
 });
 
 
+app.put('/pizza',(request,response) => 
+{
+       
+    const connection = mysql.createConnection({
+        host : 'localhost',
+        user : 'dac',
+        password : 'password',
+        database : 'db'
+    })
+    
+    console.log(request.body)
+
+    statement = `update PIZZA_ITEMS 
+    set Name = '${request.body.Name}',
+        Type = '${request.body.Type}',
+        Category = '${request.body.Category}',
+        Description = '${request.body.Description}'
+        where ID = ${request.body.ID}`;
+    
+    connection.query(statement,(error,data) => {
+
+        if (error)
+          { 
+              console.log(`error : ${error}`)
+            response.end(`error : ${error}`)
+        }
+         else
+         {   
+             console.log(`row  inserted successfully`)
+             console.table(data)
+             response.send(data)
+         }
+
+
+    })
+    
+
+
+});
+
+app.delete('/pizza',(request,response) => 
+{
+       
+    const connection = mysql.createConnection({
+        host : 'localhost',
+        user : 'dac',
+        password : 'password',
+        database : 'db'
+    })
+    
+    console.log(request.body)
+
+    statement = `delete from  PIZZA_ITEMS 
+        where ID = ${request.body.ID}`;
+    
+    connection.query(statement,(error,data) => {
+
+        if (error)
+          { 
+              console.log(`error : ${error}`)
+            response.end(`error : ${error}`)
+        }
+         else
+         {   
+             console.log(`row  inserted successfully`)
+             console.table(data)
+             response.send(data)
+         }
+
+
+    })
+    
+
+
+});
+
+
+
 app.listen(3000,'0.0.0.0', () => {
 
     console.log(`server started successfully on port 3000 `)
