3a84a29419507b859537c95d0faa5cd07f8f840c
porjesuraj <porjesuraj5@gmail.com>
2020-09-07 20:06:52 +0530

    classwork app note GET ,authenticate user by token,  done

--- Classwork/day7-7_sept/app1/routes/notes.js
+++ Classwork/day7-7_sept/app1/routes/notes.js
@@ -5,28 +5,64 @@ const utils = require('../utils')
 const { request, response } = require('express')
 const router = express.Router()
 
-router.get('/:id',(request,response) => {
+try{
+    const token = request.headers['token']
+    const data = jwt.verify(token,'123456789')
 
-    const {id} = request.params
+}
+catch (ex)
+{
+    response.status(401)
+    response.send('you are not allowed to access this API')
+}
 
-    const statement = `select contents from note where userId = ${id} `
+router.get('/',(request,response) => {
+
+  //  const {id} = request.params
+  try{
+
+
+    const token = request.headers['token']
+    const data = jwt.verify(token,'123456789')
+    const statement = `select contents from note where userId = ${data.id} `
     
      db.query(statement,(error,data) => {
 
         response.send(utils.createResult(error,data))
      })
+
+}
+catch (ex)
+{
+    response.status(401)
+    response.send('you are not allowed to access this API')
+}
+
+
+
 })
 
-router.post('/:id',(request,response) => {
+router.post('/',(request,response) => {
 
-    const {id} = request.params
+    try{
+        const token = request.headers['token']
+        const data = jwt.verify(token,'123456789')
+    //const {id} = request.params
     const{contents} = request.body
-    const statement = `insert into note  (contents,userId) values('${contents}','${id}') `
+    const statement = `insert into note  (contents,userId) values('${contents}','${data.id}') `
     
      db.query(statement,(error,data) => {
 
         response.send(utils.createResult(error,data))
      })
+    }
+    catch (ex)
+    {
+        response.status(401)
+        response.send('you are not allowed to access this API')
+    }
+
+    
 })
 
 router.put('/:id',(request,response) => {
