3183fe11f00e201cbba09f5a7b7d215bf1057d6e
porjesuraj <porjesuraj5@gmail.com>
2020-09-07 19:42:48 +0530

    classwork app note POST done with userid and id  using derefrencing,  done

--- Classwork/day7-7_sept/app1/routes/notes.js
+++ Classwork/day7-7_sept/app1/routes/notes.js
@@ -17,6 +17,17 @@ router.get('/:id',(request,response) => {
      })
 })
 
+router.post('/:id',(request,response) => {
+
+    const {id} = request.params
+    const{contents} = request.body
+    const statement = `insert into note  (contents,userId) values('${contents}','${id}') `
+    
+     db.query(statement,(error,data) => {
+
+        response.send(utils.createResult(error,data))
+     })
+})
 
 
 
