6b31c4ffe7597a6c8aa7c6b901cfe1ed6e03f80c
porjesuraj <porjesuraj5@gmail.com>
2020-09-08 19:30:53 +0530

    assignment 4 customer GET - users by id  done, using params ,after add User will try with token

--- Assignments/assignment4_08_Sept/routes/customers.js
+++ Assignments/assignment4_08_Sept/routes/customers.js
@@ -6,7 +6,7 @@ const utility = require('../utils')
 const router = express.Router()
 
 //__GET___________________________________
-router.get('/allUsers',(request,response) => {
+router.get('/',(request,response) => {
 
     const statement = `select * from customers `;
     db.query(statement,(error,data) => {
@@ -15,7 +15,15 @@ router.get('/allUsers',(request,response) => {
 
     })
 })
+router.get('/:userId',(request,response) => {
+      const {userId} = request.params
+    const statement = `select * from customers where id = ${userId} `;
+    db.query(statement,(error,data) => {
+      
+        response.send(utility.createResult(error,data))
 
+    })
+})
 
 
 
