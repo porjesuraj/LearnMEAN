e51cac8685e085a9c97840441e82921eafae129f
porjesuraj <porjesuraj5@gmail.com>
2020-09-08 19:27:05 +0530

    assignment 4 customer GET - all users done

--- Assignments/assignment4_08_Sept/routes/customers.js
+++ Assignments/assignment4_08_Sept/routes/customers.js
@@ -2,9 +2,21 @@ const express = require('express')
 const db = require('../db')
 const utility = require('../utils')
 
+
 const router = express.Router()
 
 //__GET___________________________________
+router.get('/allUsers',(request,response) => {
+
+    const statement = `select * from customers `;
+    db.query(statement,(error,data) => {
+      
+        response.send(utility.createResult(error,data))
+
+    })
+})
+
+
 
 
 //==============================================
--- Assignments/assignment4_08_Sept/server.js
+++ Assignments/assignment4_08_Sept/server.js
@@ -2,15 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const { request, response } = require('express')
+const customerRouter = require('./routes/customers')
+
 const app = express()
 
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 //middleware
+app.use('/customer',customerRouter)
 
 
 //routes
+
+
+
 app.get('/',(request,response) => {
     response.send('welcome to the application')
 })
