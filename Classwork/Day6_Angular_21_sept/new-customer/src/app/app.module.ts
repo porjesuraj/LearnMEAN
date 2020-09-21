611f47b489edb54a70f27b1f37f4cb4e1c5a3d56
porjesuraj <porjesuraj5@gmail.com>
2020-09-21 18:11:11 +0530

    angular app customer, used httpClient module, with cors middleware,for login page, to connect to db and get customer signin done

--- Classwork/Day6_Angular_21_sept/new-customer/src/app/app.module.ts
+++ Classwork/Day6_Angular_21_sept/new-customer/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 import {FormsModule} from '@angular/forms'
+import{HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {FormsModule} from '@angular/forms'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Classwork/Day6_Angular_21_sept/new-customer/src/app/login/login.component.ts
+++ Classwork/Day6_Angular_21_sept/new-customer/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-login',
@@ -9,22 +10,59 @@ export class LoginComponent implements OnInit {
 
  email = ''
  password = ''
-  constructor() { }
+
+ //class attribute
+
+ httpClient : HttpClient
+  constructor(httpClient:HttpClient) 
+  {this.httpClient = httpClient }
 
   ngOnInit(): void {
   }
 
-  OnSave()
-  {
-    console.log(`email : ${this.email}`)
-    console.log(`password : ${this.password}`)
-  }
+  
   OnCancel()
   {
     this.email = ''
     this.password = ''
     console.log(`email : ${this.email}`)
     console.log(`password : ${this.password}`)
+
+  }
+
+  OnSave()
+  {
+    console.log(`email : ${this.email}`)
+    console.log(`password : ${this.password}`)
+
+    const body = {
+
+      email : this.email,
+      password : this.password
+    }
+
+    const url = 'http://localhost:3000/customer/signin'
+
+    const request = this.httpClient.post(url,body)
+
+    request.subscribe(response => {
+
+      console.log(response)
+      if(response['status'] == 'success')
+      {
+        const data = response['data']
+        const firstName = data['firstName']
+        const lastName = data['lastName']
+        alert(`welcome ${firstName} ${lastName}`)
+      } else {
+
+        alert('invalid email or password')
+      }
+    })
+
+
+
+
 
   }
 
