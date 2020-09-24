66db3f6d6f540eb9b133665180bb6134e29e93d8
porjesuraj <porjesuraj5@gmail.com>
2020-09-24 16:05:44 +0530

    learn

--- Classwork/Day8.1_Angular_23_sept/admin-panel/src/app/app-routing.module.ts
+++ Classwork/Day8.1_Angular_23_sept/admin-panel/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
-import { UserListComponent } from './user-list/user-list.component';
-import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {path : 'login', component : LoginComponent},
-  {path : 'user-list', component : UserListComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Classwork/Day8.1_Angular_23_sept/admin-panel/src/app/app.module.ts
+++ Classwork/Day8.1_Angular_23_sept/admin-panel/src/app/app.module.ts
@@ -1,40 +1,18 @@
-import { AdminService } from './admin.service';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import {FormsModule} from '@angular/forms'
-import {HttpClientModule} from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { SignupComponent } from './signup/signup.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { OrderListComponent } from './order-list/order-list.component'
+
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    UserListComponent,
-    SignupComponent,
-    ProductAddComponent,
-    ProductListComponent,
-    OrderListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [
-    AdminService
-   
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
