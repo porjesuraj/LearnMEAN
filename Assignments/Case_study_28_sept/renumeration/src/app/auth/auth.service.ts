9d25fe51342b3dad79e0b1ebf930ae8c497db3e5
porjesuraj <porjesuraj5@gmail.com>
2020-09-28 21:49:48 +0530

    UI signup  Component done, for all , used a signle page for it, usign *ngIf

--- Assignments/Case_study_28_sept/renumeration/src/app/auth/auth.service.ts
+++ Assignments/Case_study_28_sept/renumeration/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, enableProdMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -40,4 +40,58 @@ export class AuthService {
     }
        return this.httpClient.post(this.url3 + '/signin',body)
   }
+
+   adminSignup(FirstName:string,LastName:string,Address:string,Contact:number,Email,Password)
+   {
+
+        const body = {
+          FirstName:FirstName,
+          LastName:LastName,
+          Address:Address,
+          Contact:Contact,
+          Email : Email,
+          Password : Password
+          
+        } 
+
+        return this.httpClient.post(this.url1 + '/signup',body)
+
+   }
+
+   facultySignup(FirstName:string,LastName:string,Address:string,Contact:number,Email,Password,Experience:number,Age:number)
+   {
+
+        const body = {
+          FirstName:FirstName,
+          LastName:LastName,
+          Address:Address,
+          Contact:Contact,
+          Email : Email,
+          Password : Password,
+          Experience: Experience,
+          Age:Age
+          
+        } 
+
+        return this.httpClient.post(this.url2 + '/signup',body)
+
+   }
+
+   studentSignup(FirstName:string,LastName:string,Address:string,Contact:number,Email,Password,ClassroomId:number)
+   {
+
+        const body = {
+          FirstName:FirstName,
+          LastName:LastName,
+          Address:Address,
+          Contact:Contact,
+          Email : Email,
+          Password : Password,
+          ClassroomId : ClassroomId
+        } 
+
+        return this.httpClient.post(this.url3 + '/signup',body)
+
+   }
+
 }
--- Assignments/Case_study_28_sept/renumeration/src/app/auth/signup/signup.component.ts
+++ Assignments/Case_study_28_sept/renumeration/src/app/auth/signup/signup.component.ts
@@ -1,15 +1,84 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+ 
+  FirstName = ''
+  LastName = ''
+  Address = ''
+  Contact = 1
+  Email = ''
+  Password = ''
+  ClassroomId = 1
+  Experience = 1
+  Age = 1
+  userType = 4
+  constructor(private authService : AuthService,
+              private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSave()
+  {
+    if(this.userType == 1)
+  {
+    this.authService
+    .adminSignup(this.FirstName,this.LastName,this.Address,this.Contact,this.Email,this.Password)
+    .subscribe(response => {
+      if(response['status'] == 'success')
+      {
+        alert('sucessfully Sign up')
+        this.router.navigate(['/auth/login'])
+      }
+      else
+      {
+        console.log(`error : ${response['error']}`)
+      }
+    })
+  } else if(this.userType == 2){
+    this.authService
+    .facultySignup (this.FirstName,this.LastName,this.Address,this.Contact,this.Email,this.Password,this.Experience,this.Age)
+    .subscribe(response => {
+      if(response['status'] == 'success')
+      {
+        alert('sucessfully Sign up')
+        this.router.navigate(['/auth/login'])
+      }
+      else
+      {
+        console.log(`error : ${response['error']}`)
+      }
+    })
+  }
+  else if(this.userType == 3)
+  {
+    this.authService
+    .studentSignup(this.FirstName,this.LastName,this.Address,this.Contact,this.Email,this.Password,this.ClassroomId)
+    .subscribe(response => {
+      if(response['status'] == 'success')
+      {
+        alert('sucessfully Sign up')
+        this.router.navigate(['/auth/login'])
+      }
+      else
+      {
+        console.log(`error : ${response['error']}`)
+      }
+    })
+  }
+  else
+  {
+
+  }
+
+
+
   }
 
 }
